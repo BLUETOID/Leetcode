6afb44b1f608bd304c82363dd4d9a96d3a27f1a9
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-07 11:13:04 +0530

    LeetSync: Auto-submitted solution for largest-perimeter-triangle

--- largest-perimeter-triangle/solution.cpp
+++ largest-perimeter-triangle/solution.cpp
@@ -1,1 +1,15 @@
-c
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int right = nums.size();
+        int left = 0;
+        while(right>=3){
+            int valid = nums[right-2]+nums[right-3];
+            if(valid > nums[right-1]){
+                return nums[right-1]+nums[right-2]+nums[right-3];
+            }
+            right--;
+        }
+       
+            return 0;
+    }
+};
