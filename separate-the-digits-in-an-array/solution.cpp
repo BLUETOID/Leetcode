d5d4e33042cc2fc3d5376e3e3dcfa14b03161244
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-11 08:04:35 +0530

    Fix digit separation logic in solution.cpp
    Removed redundant loop for pushing digits to result.

--- separate-the-digits-in-an-array/solution.cpp
+++ separate-the-digits-in-an-array/solution.cpp
@@ -5,10 +5,10 @@ public:
         for(int i=0;i<nums.size();i++){
             string current = to_string(nums[i]);
         }
+            for (int j = 0; j < current.length(); j++) {
                 result.push_back(current[j] - '0'); 
             }
     }
 
         return result;
-            for (int j = 0; j < current.length(); j++) {
 };
