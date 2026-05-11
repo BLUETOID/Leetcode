d34a01c5dbe7b211411c23c1760f2a448de2fa77
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-11 08:05:33 +0530

    LeetSync: Auto-submitted solution for separate-the-digits-in-an-array

--- separate-the-digits-in-an-array/solution.cpp
+++ separate-the-digits-in-an-array/solution.cpp
@@ -4,11 +4,11 @@ public:
         vector<int> result;
         for(int i=0;i<nums.size();i++){
             string current = to_string(nums[i]);
-        }
             for (int j = 0; j < current.length(); j++) {
                 result.push_back(current[j] - '0'); 
             }
-    }
+        }
 
         return result;
+    }
 };
