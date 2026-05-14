b30b52e0268d560c903882e983686346a6503147
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-14 11:02:41 +0530

    LeetSync: Auto-submitted solution for check-if-array-is-good

--- check-if-array-is-good/solution.cpp
+++ check-if-array-is-good/solution.cpp
@@ -1,14 +1,1 @@
-class Solution {
-public:
-    bool isGood(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-
-        for(int i=0;i<n-2;i++){
-            if(nums[i]==nums[i+1]) return false;
-        }
-
-        if(n==nums[n-1]+1 && nums[n-1]==nums[n-2]) return true;
-        else return false;
-    }
-};
+c
