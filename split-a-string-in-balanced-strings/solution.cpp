cc0bbd5d8b9917e9a84d223c523eb11031de4636
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-17 11:36:55 +0530

    LeetSync: Auto-submitted solution for split-a-string-in-balanced-strings

---
+++ split-a-string-in-balanced-strings/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int n = s.length();
+        int count = 0;
+        for(int i=0;i<n;i++){
+        }
+    }
+            if(s[i]=='L') checker++;
+        int checker = 0;
+            else checker--;
+            if(checker==0) count++;
+
+        return count;
