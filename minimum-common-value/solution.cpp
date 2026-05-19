f2d3dab16ebe0093a22185ab92eddef297e72ef3
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-19 12:57:06 +0530

    LeetSync: Auto-submitted solution for minimum-common-value

--- minimum-common-value/solution.cpp
+++ minimum-common-value/solution.cpp
@@ -1,17 +1,17 @@
-                return nums1[i];
-            else if (nums1[i] < nums2[j])
-                i++;
-            else
-                j++;
-        }
-
-        int j = 0;
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] == nums2[j])
-public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0;
-class Solution {
-        return -1;
-    }
-};
+1class Solution {
+2public:
+3    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+4        int i = 0;
+5        int j = 0;
+6        while (i < nums1.size() && j < nums2.size()) {
+7            if (nums1[i] == nums2[j])
+8                return nums1[i];
+9            else if (nums1[i] < nums2[j])
+10                i++;
+11            else
+12                j++;
+13        }
+14
+15        return -1;
+16    }
+17};
