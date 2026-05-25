6112255c343b44007e8a33e131942939a983cd38
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-05-25 18:08:12 +0530

    LeetSync: Auto-submitted solution for median-of-two-sorted-arrays

--- median-of-two-sorted-arrays/solution.cpp
+++ median-of-two-sorted-arrays/solution.cpp
@@ -17,3 +17,13 @@ public:
                 j++;
             }
         }
+        sort(mergedArray.begin(),mergedArray.end());
+
+        if((n+m) %2 != 0){
+            return mergedArray[(n+m)/2];
+        }
+        else{
+            return (mergedArray[(n+m)/2]+mergedArray[(n+m)/2-1])/2.0;
+        }
+    }
+};
