b8e10b4a734bdd828188622a493fd0b2991e03ad
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-01 07:59:24 +0530

    LeetSync: Auto-submitted solution for two-sum

--- two-sum/solution.cpp
+++ two-sum/solution.cpp
@@ -1,2 +1,12 @@
 
-c
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int left = 0;
+        int right = 1;
+        for(int left=0;left<nums.size();i++){
+            if(nums[left]+nums[right])
+        }
+        return {left,right};
+    }
+};
