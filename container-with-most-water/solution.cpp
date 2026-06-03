841b2c203cf3b800fffa46890ec292b6f59007fc
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-03 11:40:44 +0530

    LeetSync: Auto-submitted solution for container-with-most-water

--- container-with-most-water/solution.cpp
+++ container-with-most-water/solution.cpp
@@ -1,2 +1,20 @@
 
-c
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maxArea = 0;
+        int left=0;
+        int right = n -1;
+        while(left<right){
+            int hight = min(height[left],height[right]);
+            int width = right - left;
+            int area = hight * width;
+            maxArea = max(maxArea,area);
+
+            if(hei)
+        }
+
+        return maxArea;
+    }
+};
