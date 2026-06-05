aa14d97356454dcde75f7d276c3ad0c48b0bb976
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-05 09:49:31 +0530

    LeetSync: Auto-submitted solution for longest-common-prefix

--- longest-common-prefix/solution.cpp
+++ longest-common-prefix/solution.cpp
@@ -1,2 +1,16 @@
 
-c
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string common_prefix=strs[0];
+
+        for(int i=1;i<strs.length();i++){
+            int j=0;
+            while(j<min(common_prefix.length(),strs[i].length())){
+                if(common_prefix[j] != strs[i][j]) break;
+                j++;
+            }
+            common_prefix=common_prefix
+        }
+    }
+};
