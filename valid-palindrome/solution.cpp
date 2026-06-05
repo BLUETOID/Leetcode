fab8fa78c7da23234c2fd558968540df57a18948
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-05 09:56:11 +0530

    LeetSync: Auto-submitted solution for valid-palindrome

--- valid-palindrome/solution.cpp
+++ valid-palindrome/solution.cpp
@@ -1,16 +1,2 @@
 
-class Solution {
-public:
-    bool isPalindrome(string s) {
-        string lowercase="";
-        string reversed ="";
-        for(char c:s){
-            if(isalnum(c)) lowercase += tolower(c);
-        }
-        int n = lowercase.length();
-        for(int i=n-1;i>=0;i--){
-            reversed+=lowercase[i];
-        }
-        return lowercase==reversed;
-    }
-};
+c
