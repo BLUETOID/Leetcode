cf7b1d30cd89bf19d5d357bb74e5bf63e858a9e0
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-08 19:04:02 +0530

    LeetSync: Auto-submitted solution for ransom-note

--- ransom-note/solution.cpp
+++ ransom-note/solution.cpp
@@ -1,16 +1,13 @@
 
-class Solution {
-public:
-    bool canConstruct(string ransomNote, string magazine) {
-        
-        unordered_map<char,int>freq1;
-        for(auto x:magazine){
             freq1[x]++;
         }
 
-        for(auto x :ransomNote ){
+        for (auto x : ransomNote) {
             freq1[x]--;
-            if(freq1[x] < 0) return false;
+            if (freq1[x] < 0)
+                return false;
         }
 
         return true;
+    }
+};
