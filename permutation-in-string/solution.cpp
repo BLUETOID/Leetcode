0f3fe6a44f8065c80eb9c72e00cfa57da02df9ac
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-09 09:25:29 +0530

    LeetSync: Auto-submitted solution for permutation-in-string

--- permutation-in-string/solution.cpp
+++ permutation-in-string/solution.cpp
@@ -1,2 +1,21 @@
 
-c
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+
+        vector<char>f1(s1.begin(),s1.end());
+        sort(f1.begin(),f1.end());
+
+        for(int i=0;i<s2.size()-s1.size();i++){
+            vector<char>f2;
+            for(int j=i;j<i+s1.size();j++){
+                f2.push_back(s2[j]);
+            }
+            sort(f2.begin(),f2.end());
+            if(f1==f2) return true;
+        }
+
+        return false;
+    }
+};
