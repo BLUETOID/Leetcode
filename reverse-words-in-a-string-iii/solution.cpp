597e8ccdf7dd74047695346dc4af16d7700ebf1f
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-11 07:12:47 +0530

    LeetSync: Auto-submitted solution for reverse-words-in-a-string-iii

---
+++ reverse-words-in-a-string-iii/solution.cpp
@@ -0,0 +1,16 @@
+
+    string reverseWords(string s) {
+        int left = 0;
+        int right = 0;
+        while(right<s.length()){
+            if(s[right] == ' '){
+                reverse(s.begin()+left , s.begin()+right);
+                left = right+1;
+            }
+            right++;
+        }
+        reverse(s.begin()+left,s.begin()+right);
+
+        return s;
+    }
+};
