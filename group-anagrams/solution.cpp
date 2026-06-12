708c743a9fdd70cc86af535c44ac9f2d6523e5a5
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-12 06:52:35 +0530

    LeetSync: Auto-submitted solution for group-anagrams

---
+++ group-anagrams/solution.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>>mp;
+        for(auto x : strs){
+            string key = x;
+            sort(key.begin(),key.end());
+            mp[key].push_back(x);
+        }
+
+        vector<vector<string>>result;
+        for(auto &[keys,value]:mp){
+            result.push_back(value);
+        }
+
+        return result;
+    }
+};
