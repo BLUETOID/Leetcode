fd114041e83f66883a103991333d9d074515346b
Rishikesh Tomar <148844113+BLUETOID@users.noreply.github.com>
2026-06-12 07:53:54 +0530

    LeetSync: Auto-submitted solution for longest-palindromic-substring

--- longest-palindromic-substring/solution.cpp
+++ longest-palindromic-substring/solution.cpp
@@ -2,12 +2,12 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
+        int n = s.length();
+        int maxLength = 0 ;
         int start = 0;
-        int maxLength = 1;
 
         auto expand = [&](int l,int r){
-            while(l>=0 && r<n && s[l]==s[r]){
+            while(l>=0 && r < n && s[l]==s[r]){
                 if(r-l+1 > maxLength){
                     maxLength = r-l+1;
                     start = l;
@@ -16,12 +16,3 @@ public:
                 r++;
             }
         };
-
-        for(int i=0;i<n;i++){
-            expand(i,i);
-            expand(i,i+1);
-        }
-
-        return s.substr(start,maxLength);
-    }
-};
